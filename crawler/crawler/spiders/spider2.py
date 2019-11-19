c5e25a2ccd2c36bc5eb15bc255d7bf004f06eb86
tarassque9 <taras.sabat9@gmail.com>
2019-11-19 23:35:21 +0200

    third commit

--- crawler/crawler/spiders/spider2.py
+++ crawler/crawler/spiders/spider2.py
@@ -14,22 +14,27 @@ class StartupsSpider2(scrapy.Spider):
 
     def get_page(self, response):
         jsonresponse = json.loads(response.body_as_unicode())
+        result_list = jsonresponse['data']['list']
         list_startups = []
-        for el in jsonresponse['data']['list']:
-            company_name = el['name']
-            profile_url = 'https://e27.co/%s' % el['slug'] + '\n'
-            company_website_url = el['metas']['website']
-            location = el['location'][0]['text']
-            tags = el['market']
+        for el in result_list:
+            for k,v in el.items():
+                if type(v) == str:
+                    if el[k] == '[[]]':
+                        el[k] = ' '
+        for e in result_list:
+            company_name = e['name']
+            profile_url = 'https://e27.co/%s' % e['slug']
+            company_website_url = e['metas']['website']
+            raised_amount = e['raised_amount']
+            location = e['location'][0]['text']
+            tags = e['market']
             totalstartupcount = jsonresponse['data']['totalstartupcount'] #this is number of all profile startups on this site. But i don't know how to convey this value in parce method
             short_description = el['metas']['short_description']
             information = {'company_name': company_name, 'profile_url': profile_url, 
-                    'company_web_site_url': company_website_url, 
+                    'company_web_site_url': company_website_url, 'raised_amount': raised_amount,
                     'location': location, 'tags': tags, 'short_description': short_description}
             
-
             list_startups.append(information)
-            
         keys = list_startups[0].keys()
         with open('profiles_info.csv', 'w') as myfile:
             dict_writer = csv.DictWriter(myfile, keys)
